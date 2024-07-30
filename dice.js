1d489dd4ec52ca33506c823f278c9340df979359
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-07-30 23:26:49 +0100

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,20 +1,25 @@
 window.onload=()=>{
     
   let dicePointer = document.querySelector(".dicePointer")  
+  let range = document.querySelector(".range")
     let fig = document.querySelector(".fig")
  let rollBtn = document.querySelector(".roll")
     
-    rollBtn.addEventListener("click",()=>{
-        
-        
+    
+    
+            rollBtn.addEventListener("click",()=>{
+         
     let randomNum = Math.floor(Math.random() * 100) 
     fig.innerHTML = randomNum 
     dicePointer.style.left = `${randomNum}%`
-   
-    console.log(randomNum )
-        
-    })
     
+    if(range.value < randomNum){
+        console.log("you lose")
+    }else{
+        console.log("you win")
+    }
+    })
+  
     
     
 }
